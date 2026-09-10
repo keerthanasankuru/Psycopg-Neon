015797a7468d911326ba633a81c30b4a68be3c9b
keerthanasankuru <147173030+keerthanasankuru@users.noreply.github.com>
2026-09-10 17:56:09 +0530

    Update PracticeQuestion2.py

--- PracticeQuestion2.py
+++ PracticeQuestion2.py
@@ -4,35 +4,36 @@ cur=conn.cursor()
 
 
 def project():
-    entry1=input("Enter values to insert seperated by space:").split()
-    entry2=input("Enter values to insert seperated by space:").split()
-    entry3=input("Enter values to insert seperated by space:").split()
-    # entry1=[1111,"G",77.12]
-    # entry2=[2222,"U",99.0]
-    # entry3=[4444,"J",33.7]
-    entries=[]
-    entries.append(entry1)
-    entries.append(entry2)
-    entries.append(entry3)
+    entries = []
+    n=int(input("No of values you want to enter:"))
+    for i in range(n):
+        entry = input("Enter values: ").split()
+        entries.append(entry)
+    # entry1=input("Enter values to insert seperated by space:").split()
+    # entry2=input("Enter values to insert seperated by space:").split()
+    # entry3=input("Enter values to insert seperated by space:").split()
+    # entry1=[1111,"G","77.12"]
+    # entry2=[2222,"U","99.0"]
+    # entry3=[4444,"J","33.7"]
+    
+    # entries.append(entry1)
+    # entries.append(entry2)
+    # entries.append(entry3)
     for i in range(len(entries)):
-        entries[i][2]=float(entries[i][2])
-        int_part=int(entries[i][2])
-        decimal=round((entries[i][2]-int_part),2)
-        print(decimal)
-        if decimal>0.12:
-            print("Months In Age cannot be greater than 12")
-            return
-        if decimal==0.12:
-            
+        int_part=int(float(entries[i][2]))
+        year,month=entries[i][2].split(".")
+        if int(month)>12:
+            return "Invalid Age,Check Again."
+        if int(month)==12:
             entries[i][2]=int_part+1
-            print(entries[i][2])
+
         
     cur.execute("""CREATE TABLE USERS (ID INT PRIMARY KEY,
     NAME VARCHAR(50),AGE FLOAT)""")
     conn.commit()
-    check_condition="""SELECT id FROM USERS WHERE ID=%s"""
     print("table created")
     for i in range(len(entries)):
+        check_condition="""SELECT id FROM USERS WHERE ID=%s"""
 
         cur.execute(check_condition,(entries[i][0],))
         if cur.fetchone() is None:
